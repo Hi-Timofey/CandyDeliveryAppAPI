dab091673b7d42ce871fe576b83f4c1aa0b9be75
Hi-Timofey <Katkov0103@yandex.ru>
2021-03-28 23:29:59 +1000

    Fixes for installation script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 # Installing virtual env
 python3 -m pip install virtualenv
-
-# Creating env
+# Creating env and load into
 python3 -m virtualenv .flask_env
+source .flask_env/bin/activate
 
 # Installing requirements.txt
 python3 -m pip install -r requirements.txt
